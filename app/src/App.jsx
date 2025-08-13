2806edc4e0beb3f1291c4ad588cfd784507378b0
Kawere C. Wagaba <kawerewagaba@gmail.com>
2025-08-13 15:40:27 +0300

    feat: enable editing: some interesting design patterns

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -4,6 +4,7 @@ import './App.css'
 function App() {
   const [tasks, setTasks] = useState([])
   const [input, setInput] = useState('')
+  const [edit, setEdit] = useState({ mode: false, taskId: null })
 
   useEffect(() => {
     fetch('http://localhost:3001/tasks')
@@ -18,7 +19,26 @@ function App() {
       body: JSON.stringify({ text: input })
     })
     .then(res => res.json())
-    .then(data => setTasks([...tasks, data]))
+    .then(data => {
+      setTasks([...tasks, data])
+      setInput('')
+    })
+  }
+
+  const editTask = (id) => {
+    fetch(`http://localhost:3001/tasks/${id}`, {
+      method: 'PUT',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ text: input })
+    })
+    .then(res => res.json())
+    .then(data => {
+      const filteredTasks = tasks.filter(task => task.id !== id)
+
+      setTasks([...filteredTasks, data])
+      setEdit({ mode: false, taskId: null })
+      setInput('')
+    })
   }
 
   const deleteTask = (id) => {
@@ -33,13 +53,34 @@ function App() {
     <>
       <div>
         <input type='text' value={input} onChange={e => setInput(e.target.value)} />
-        <button onClick={addTask}>add task</button>
+        {
+          edit.mode ?
+          <button onClick={() => editTask(edit.taskId)}>edit task</button>
+          :
+          <button onClick={addTask}>add task</button> 
+        }
       </div>
       <div>
         {
           tasks.map(task => (
             <div key={task.id}>
-              {task.text}
+              { task.text }
+              {
+                edit.mode ?
+                <button onClick={() => {
+                  setEdit({ mode: false, taskId: null })
+                  setInput('')
+                }}>
+                  cancel
+                </button>
+                :
+                <button onClick={() => {
+                  setEdit({ mode: true, taskId: task.id })
+                  setInput(task.text)
+                }}>
+                  edit task
+                </button>
+              }
               <button onClick={() => deleteTask(task.id)}>delete task</button>
             </div>
           ))
